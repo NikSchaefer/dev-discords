2cd2ae3c3fa1b24ad8e764b07d5b24f73be4b796
Nik Schaefer <nikkschaefer@gmail.com>
2021-01-20 15:29:31 -0600

    Icons + Remove Jest

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 // eslint-disable-next-line import/no-default-export
 export default function FourOhFour(): JSX.Element {
 	useEffect(() => {
-		window.location.href = "/";
+		window.location.href = "/"; // redirect
 	});
 	return <h1>Redirecting...</h1>
 }
